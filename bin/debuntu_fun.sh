45cdb8729e7890a745ab3831a8b7fc4f1766158b
Thomas Schank <DrTom@schank.ch>
2013-10-02 14:03:54 +0200

    Updated chromedriver to 2.4

--- bin/debuntu_fun.sh
+++ bin/debuntu_fun.sh
@@ -2,7 +2,7 @@ function debuntu_ci_chromedriver_install {
 TMDIR=`mktemp -d`
 cd $TMDIR
 MACHINE_BITS=`uname -m | cut -d '_' -f 2`
-curl -s -L "https://chromedriver.googlecode.com/files/chromedriver_linux${MACHINE_BITS}_2.3.zip" > chromedriver.zip
+curl -s -L "http://chromedriver.storage.googleapis.com/2.4/chromedriver_linux${MACHINE_BITS}.zip" > chromedriver.zip
 unzip chromedriver.zip
 mv chromedriver ~/bin
 cd
