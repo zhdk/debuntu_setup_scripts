d3631258cd99f65cb68f31b53baa2ba8c04242a1
Thomas Schank <DrTom@schank.ch>
2013-10-18 10:56:02 +0200

    Simplified torquebox install
    start/stop dispatcher between Ubuntu/Debian is not needed

--- src/torquebox/install_3.0.0.sh
+++ src/torquebox/install_3.0.0.sh
@@ -5,33 +5,23 @@ TB_ROOT="/opt/torquebox-3.0.0"
 TMP_FILE="/tmp/torquebox-${TB_VERSION}.zip"
 TB_LINK="/opt/torquebox"
 
-
-### stopping torquebox
-if debuntu_system_is_ubuntu; then
-        stop torquebox
-        MATCHER='java.*jar.*torquebox'
-        pgrep -f "$MATCHER"
-        if [ $? -ne 0 ]; then
-          sleep 10
-          pkill -SIGTERM -f "$MATCHER"
-        fi
-        pgrep -f "$MATCHER"
-        if [ $? -ne 0 ]; then
-          sleep 10
-          pkill -SIGKILL -f "$MATCHER"
-        fi
-        stop torquebox
-else
-        service torquebox stop
+service torquebox stop
+MATCHER='java.*jar.*torquebox'
+pgrep -f "$MATCHER"
+if [ $? -ne 0 ]; then
+  sleep 10
+  pkill -SIGTERM -f "$MATCHER"
 fi
-
+pgrep -f "$MATCHER"
+if [ $? -ne 0 ]; then
+  sleep 10
+  pkill -SIGKILL -f "$MATCHER"
+fi
 
 ### installing prerequisites
 debuntu_jvm_open_jdk_install
 
-
 ### do it 
-
 adduser --disabled-password -gecos "" torquebox
 if [ ! -f ${TMP_FILE} ]; then
   curl -L "$TB_URL" > "$TMP_FILE"
@@ -46,9 +36,4 @@ debuntu_torquebox_setup_env_loader
 debuntu_torquebox_setup_logrotate
 debuntu_torquebox_setup_upstart
 
-
-if debuntu_system_is_ubuntu; then
-        start torquebox
-else
-        service torquebox start
-fi
+service torquebox start
